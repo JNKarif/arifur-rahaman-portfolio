1d9161261db50a469b98f1258f79019ed048e637
JNKarif <arif.rahman041@gmail.com>
2022-12-11 23:44:44 +0600

    final

--- src/Pages/Shared/NavBar/NavBar.js
+++ src/Pages/Shared/NavBar/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
 
     <div>
 
-      <div className="navbar bg-base-100">
+      <div className="navbar bg-gradient-to-r from-pink-100 to-purple-300">
         <div className="navbar-start">
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost lg:hidden">
