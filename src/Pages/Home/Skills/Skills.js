915d33ae900f5c3c3691a86deffea6de019884da
JNKarif <arif.rahman041@gmail.com>
2022-12-15 01:03:14 +0600

    skills-edited

--- src/Pages/Home/Skills/Skills.js
+++ src/Pages/Home/Skills/Skills.js
@@ -9,78 +9,72 @@ const Skills = () => {
     // const {mern}= logos;
 
     return (
-        <div className='grid lg:grid-cols-2  bg-gradient-to-r from-pink-300 to-purple-600 p-20 '>
+        <div className='bg-gradient-to-r from-pink-300 to-purple-600 p-20 '>
+            <div className="grid lg:grid-cols-2 bg-gradient-to-r from-pink-300 to-purple-600 p-5  ">
 
+                <div className=''>
+                    <p className='text-3xl font-bold mb-4 '>Skills</p>
 
+                    <div className='grid grid-cols-2 gap-5'>
 
-            <div className='p-5'>
+                        <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
+                            <figure><img src={mern} alt="Shoes" /></figure>
 
-                <p className='text-3xl font-bold mb-4 '>Skills</p>
+                        </div>
 
+                        <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
+                            <figure><img src={js} alt="Shoes" /></figure>
 
-                <div className='grid lg:grid-cols-2 gap-16'>
-                    <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
-                        <figure><img src={mern} alt="Shoes" /></figure>
+                        </div>
 
+                        <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
+                            <figure><img src={tailbs} alt="Shoes" /></figure>
+
+                        </div>
+
+                        <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
+                            <figure><img src={git} alt="Shoes" /></figure>
+
+                        </div>
                     </div>
 
-                    <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
-                        <figure><img src={js} alt="Shoes" /></figure>
-
-                    </div>
-
-                    <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
-                        <figure><img src={tailbs} alt="Shoes" /></figure>
-
-                    </div>
-
-                    <div className="card w-64 h-36 rounded-xl shadow-xl  glass">
-                        <figure><img src={git} alt="Shoes" /></figure>
-
-                    </div>
                 </div>
 
 
 
 
-                {/* <p className='mt-1 text-white text-xl font-bold '>
-                    Expertise: HTML5, CSS3, Tailwind CSS, Bootstrap <br /> Comfortable: JavaScript, React, MongoDB <br/>
-                    Familiar: React Native, JWT, Node.js, Express.js <br /> Tools: Github, Netlify, Firebase, VS Code
-                </p> */}
+
+                <div className='flex justify-center align-center p-10'>
+                    <ul className=''>
+                        <li><p className='text-2xl'>JavaScript</p>
+                            <span><progress className="progress progress-info w-56" value="70" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>React</p>
+                            <span><progress className="progress progress-info w-56" value="70" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>HTML5</p>
+                            <span><progress className="progress progress-info w-56" value="90" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>CSS3</p>
+                            <span><progress className="progress progress-info w-56" value="90" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>Tailwind CSS</p>
+                            <span><progress className="progress progress-info w-56" value="100" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>Bootstrap</p>
+                            <span><progress className="progress progress-info w-56" value="100" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>MongoDB</p>
+                            <span><progress className="progress progress-info w-56" value="50" max="100"></progress></span>
+                        </li>
+                        <li><p className='text-2xl'>MERN</p>
+                            <span><progress className="progress progress-info w-56" value="40" max="100"></progress></span>
+                        </li>
+                    </ul>
+                </div>
+
+
             </div>
-
-
-            <div className='flex justify-center align-center p-10'>
-                <ul className=''>
-                    <li><p className='text-2xl'>JavaScript</p>
-                        <span><progress className="progress progress-info w-56" value="70" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>React</p>
-                        <span><progress className="progress progress-info w-56" value="70" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>HTML5</p>
-                        <span><progress className="progress progress-info w-56" value="90" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>CSS3</p>
-                        <span><progress className="progress progress-info w-56" value="90" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>Tailwind CSS</p>
-                        <span><progress className="progress progress-info w-56" value="100" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>Bootstrap</p>
-                        <span><progress className="progress progress-info w-56" value="100" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>MongoDB</p>
-                        <span><progress className="progress progress-info w-56" value="50" max="100"></progress></span>
-                    </li>
-                    <li><p className='text-2xl'>MERN</p>
-                        <span><progress className="progress progress-info w-56" value="40" max="100"></progress></span>
-                    </li>
-                </ul>
-            </div>
-
-
-
         </div>
 
 
